ae8aa88a9616441d825a1bf676115caecd5cc5ae
Kali <theson3822@gmail.com>
2022-04-04 06:49:20 -0400

    Crick commit

--- WebProject/controllers/users_auth.js
+++ WebProject/controllers/users_auth.js
@@ -4,68 +4,113 @@ const jwt = require('jsonwebtoken');
 const dotenv = require('dotenv');
 dotenv.config();
 let cookies = {};
+let loginProcess;
 
 const users_auth = {
+  check_cookie: function(req,res,key_string){
+    return new Promise((resolve,reject)=>{
+      if(req.headers.cookie.search(key_string) !== -1){
+        //usercookie=....
+        const startIndex = req.headers.cookie.search(key_string) + key_string.length + 1;
+        //startIndex=11
+        const received_cookie = req.headers.cookie.slice(startIndex);
+        jwt.verify(received_cookie,process.env.REFRESH_KEY,err => {
+          if(err){
+            console.log(err);
+            res.send('COOKIE WRONG');
+            reject(err)
+          }else if(cookies[received_cookie] !== undefined){
+            console.log(cookies);
+            res.send('LOGIN WITH COOKIE SUCCESSFULLY');
+            resolve(received_cookie);
+          }
+        })
+      }
+    })
+  },
   create_refresh_token: function(payload){
     return jwt.sign(payload, process.env.REFRESH_KEY);
   },
   register: async function(req,res){
-    const {username,password,email,phone} = req.body;
-    const check_username = await users_auth_schema.findOne({username: username});
+    //EMAIL IS REQUIRED
+    if(req.body.email === ''){
+      return res.status(404).send('PLEASE SEND YOUR EMAIL').end();
+    }
+    const check_username = await users.findOne({email: req.body.email});
 
     if(check_username){
-      res.send('USER IS ALREADY EXISTS');
-      res.end();
+      return res.send('USER IS ALREADY EXISTS').end();
     }
 
     const salt = await bcrypt.genSalt(10);
-    const hashed_password = await bcrypt.hash(password,salt);
+    const hashed_password = await bcrypt.hash(req.body.password,salt);
     const create_User = await new users({
-      username: username,
-      password: password,
-      email: email,
-      phone: phone
+      username: req.body.username,
+      password: hashed_password,
+      email: req.body.email,
+      phone: req.body.phone
     });
     create_User.save()
-    .then(()=>{
+    .then(async function(){
       //SEND SMS MESSAGE
+      const new_User = await users.findOne({email: req.body.email});
+      res.json({
+        newUser: `${JSON.stringify(new_User)}`
+      })
       console.log('CREATE USER SUCCESS');
     })
     .catch(e => {
+      console.log(e);
       res.json({
         message: 'CANNOT CREATE NEW USER'
       });
     })
   },
   login: async function(req,res){
-    jwt.verify(req.cookies.usercookie, process.env.REFRESH_KEY, (err) =>{
-      if(err){
-        res.status(404).send('BAD REQUESTS');
-      }
-    });
-    if([req.cookies.usercookie] !== null){
-      next();
+    //Check if cookie is valid
+    if(req.headers.cookie !== undefined){
+      users_auth.check_cookie(req,res,'usercookie')
+      .then((received_cookie)=>{
+        console.log('END CONNECTION HERE'); //next()
+        clearTimeout(loginProcess); // END PROGRAM HERE IMMEDIATELY
+      })
+      .catch((e)=>{
+        console.log(e);
+        clearTimeout(loginProcess);
+      })
     };
-    const user_info = req.body;
-    const check_user = await users.findOne({username: user_info.username});
-    if(!check_user){
-      res.json(`CANNOT FIND USER ${username}`);
-    }
-    const check_password = await bcrypt.compare(check_user.password,user_info.password);
-    if(!check_password){
-      res.json(`USER ${username} SUBMITS WRONG PASSWORD`);
-    }
-    const refresh_token = users_auth_schema.create_refresh_token(user_info);
-    res.cookie('usercookie',refresh_token,{
-      httpOnly: false,
-      secure: false
-    });
-    cookies[refresh_token] = JSON.stringify(check_user);
+    loginProcess = setTimeout(async ()=>{
+      console.log('loginProcess START');
+      const login_user = await users.findOne({email: req.body.email});
+      if(!login_user){
+        res.json(`CANNOT FIND USER ${req.body.username}`);
+      }
+      const check_password = await bcrypt.compare(req.body.password,login_user.password);
+      if(!check_password){
+        res.json(`USER ${req.body.username} SUBMITS WRONG PASSWORD`).end();
+      }
+      const refresh_token = users_auth.create_refresh_token(req.body);
+      cookies[refresh_token] = JSON.stringify(req.body);
+      res.cookie('usercookie',refresh_token,{
+        httpOnly: false,
+        secure: false
+      });
+      console.log('loginProcess END');
+      res.end();
+    },0)
   },
-  logout: function(req,res){
-    delete cookies[req.cookies.usercookie];
-    res.clearCookie('usercookie');
-    res.end();
+  logout: async function(req,res){
+    if(req.headers.cookie !== undefined){
+      users_auth.check_cookie(req,res,'usercookie')
+      .then((received_cookie)=>{
+        delete cookies[received_cookie];
+        res.clearCookie('usercookie').end();
+      })
+      .catch((e)=>{
+        console.log(e);
+        return res.end();
+      })
+    }
   }
 }
 
--- WebProject/server.js
+++ WebProject/server.js
@@ -3,6 +3,7 @@ const app = express();
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
 const users_auth = require('./routers/users_auth.js');
+const bodyParser = require('body-parser');
 dotenv.config();
 
 mongoose.connect(process.env.CLOUD_DB)
@@ -12,8 +13,13 @@ mongoose.connect(process.env.CLOUD_DB)
 .catch(e => {
   console.log(e);
 })
-
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded())
+app.get('/', (req,res)=>{
+  res.send('HOME PAGE');
+});
 app.use('/users', users_auth);
+
 
 app.listen(process.env.PORT, ()=>{
   console.log('SERVER IS WORKING');
